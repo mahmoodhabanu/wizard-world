b54d7dca38eca9bd654af951ab69c7c743d13456
mahmoodha@bytemark.co <77284238+Mahmoodhasadique@users.noreply.github.com>
2025-10-13 14:04:04 +0530

    Removed Unused imports

--- WizardWorld/app/src/main/java/com/example/wizardworld/MainActivity.kt
+++ WizardWorld/app/src/main/java/com/example/wizardworld/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.wizardworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.wizardworld.ui.navigation.AppNavGraph
 import com.example.wizardworld.ui.theme.WizardWorldTheme
--- WizardWorld/houses-presentation/src/main/java/com/example/houses_presentation/houselist/HouseListScreen.kt
+++ WizardWorld/houses-presentation/src/main/java/com/example/houses_presentation/houselist/HouseListScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.core_common.UIState
 import com.example.core_common.toMessage
 import com.example.houses_domain.model.House
